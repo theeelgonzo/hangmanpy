802cf40a9ac61069b4d6a40657bf5773f3fbcba0
Jordan Godwin <jordan.godwin13@gmail.com>
2023-05-13 21:48:21 -0500

    Add game loop

---
+++ hangman.py
@@ -0,0 +1,14 @@
+count = 6
+alreadyGuessed = []
+
+def playGame(secretWord):
+    encWord = len(secretWord) * '*'
+    # print(encWord)
+    if (count > 0) & (encWord != secretWord):
+        print('The game is afoot!')
+
+
+
+
+
+playGame('poodle')
