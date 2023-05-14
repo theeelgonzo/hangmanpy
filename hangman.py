89e5f458395e293154fa62b199b96eda4edaa6a3
Jordan Godwin <jordan.godwin13@gmail.com>
2023-05-13 22:12:17 -0500

    add game logic and fail to make game run

--- hangman.py
+++ hangman.py
@@ -2,10 +2,26 @@ count = 6
 alreadyGuessed = []
 
 def playGame(secretWord):
-    encWord = len(secretWord) * '*'
+    secretWord = list(secretWord)
+    encWord = list(len(secretWord) * '*') 
     # print(encWord)
-    if (count > 0) & (encWord != secretWord):
-        print('The game is afoot!')
+    while (count > 0) & (encWord != secretWord):
+        #print('The game is afoot!')
+        print(str(encWord))
+        nextGuess = input('Guess a letter... \n')
+        for char in range(len(secretWord)):
+            if nextGuess == secretWord[char]:
+                encWord[char] = secretWord[char]
+                continue
+            else:
+                break
+
+    if (count > 0 ) & (encWord == secretWord):
+        print('You win!')
+        
+    else:
+        print('You lose!')
+    
 
 
 
